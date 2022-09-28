cb4643b5a0c7da1f315885b246983fcd5d13333c
Joel Singh <joelsingh788@gmail.com>
2022-09-28 12:53:14 -0400

    Change .main to respective page on each button click

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ document.querySelector("#content").append(createInitialPage());
 function wipeMain() {
   document.querySelector(".main").innerHTML = "";
 }
+
+document.querySelector(".home-btn").addEventListener("click", () => {
+  wipeMain();
+  document.querySelector(".main").append(createHomePage());
+});
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+  wipeMain();
+  document.querySelector(".main").append(createMenuPage());
+});
+
+document.querySelector(".contact-btn").addEventListener("click", () => {
+  wipeMain();
+  document.querySelector(".main").append(createContactPage());
+});
